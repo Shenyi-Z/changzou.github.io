4a4bcb074c81e77c1185b4bef0d55a03b44b917f
Shenyi-Z <115690021+Shenyi-Z@users.noreply.github.com>
2026-03-11 10:20:13 +0000

    Deploying to gh-pages from @ Shenyi-Z/shenyi-z.github.io@972e1d89d59c4a6671b5d3c691d1d548e96a4410 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -518,14 +518,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%73%68%65%6E%79%69%7A%6F%75@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-rss',
@@ -539,14 +532,21 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=yWm0cJsAAAAJ", "_blank");
         },
       },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://github.com/Shenyi-Z", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/chang-zou-9532633a4", "_blank");
         },
       },{
       id: 'light-theme',
